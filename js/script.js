1b01aaaff1b302b0b9fe337b0b0b27367eea70d0
shebnem reshidova <shabnamrashidova90@gmail.com>
2022-08-27 15:21:20 +0400

    ..

--- js/script.js
+++ js/script.js
@@ -1,90 +1,93 @@
-$('.banner-carousel').owlCarousel({
-    loop: true,
-    margin: 0,
-    dots: true,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
+$(document).ready(function () {
+    $('.banner-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
+            }
 
         }
+    })
 
-    }
-})
+    $('.blog-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
 
-$('.blog-carousel').owlCarousel({
-    loop: true,
-    margin: 20,
-    responsiveClass: true,
-
-    responsive: {
-        0: {
-            items: 1,
-            dots: true,
-        },
-        768: {
-            items: 2,
-        },
-        1200: {
-            items: 3,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+            },
+            768: {
+                items: 2,
+            },
+            1200: {
+                items: 3,
+            }
         }
-    }
-})
+    })
 
-$('.partners-carousel').owlCarousel({
-    loop: true,
-    margin: 20,
-    responsiveClass: true,
+    $('.partners-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
 
-    responsive: {
-        0: {
-            items: 1,
-            dots: true,
-        },
-        768: {
-            items: 2,
-        },
-        1200: {
-            items: 5,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+            },
+            768: {
+                items: 2,
+            },
+            1200: {
+                items: 5,
+            }
         }
-    }
+    })
+
+    AOS.init();
+    $(window).on("scroll", () => {
+        if ($(this).scrollTop() >= 120) {
+            $(".general-header").addClass("header-fixed")
+            $(".general-header-1 .header-logo img").attr("src", "./images/logo2/Group 8.png")
+
+            $(".scroll-btn").addClass("show")
+            $(".scroll-btn").on("click", () => {
+                $(window).scrollTop(0)
+            })
+
+        } else {
+            $(".general-header").removeClass("header-fixed")
+            $(".general-header-1 .header-logo img").attr("src", "./images/Group 8.png")
+            $(".scroll-btn").removeClass("show")
+        }
+    })
+
+    $(".nav-menu-btn").on('click', () => {
+        $(".header-mobile-menu").addClass("show");
+    })
+    $(".close-menu").on('click', () => {
+        $(".header-mobile-menu").removeClass("show");
+    })
+$(".nav-menu-search").on('click',()=>{
+    $(".search-wrapper").toggleClass("active")
+    $(".nav-menu-search i").toggleClass("fa-times")
 })
 
-AOS.init();
-$(window).on("scroll", () => {
-    if ($(this).scrollTop() >= 120) {
-        $(".general-header").addClass("header-fixed")
-        $(".general-header-1 .header-logo img").attr("src","./images/logo2/Group 8.png")
 
-        $(".scroll-btn").addClass("show")
-        $(".scroll-btn").on("click", () => {
-            $(window).scrollTop(0)
-        })
+    $(".gallery a").attr("data-fancybox", "mygallery");
+    $(".gallery a").each(function () {
+        $(this).attr("data-caption", $(this).find("img").attr("alt"));
+        $(this).attr("title", $(this).find("img").attr("alt"));
+    });
 
-    } else {
-        $(".general-header").removeClass("header-fixed")
-        $(".general-header-1 .header-logo img").attr("src","./images/Group 8.png")
-        $(".scroll-btn").removeClass("show")
-    }
-})
-$(".nav-menu-btn").on('click', () => {
-    $(".header-mobile-menu").addClass("show");
-})
-$(".close-menu").on('click', () => {
-    $(".header-mobile-menu").removeClass("show");
-})
-
-    $(document).ready(function() {
     $(".gallery a").fancybox();
-  });
-  $(document).ready(function() {
-$(".gallery a").attr("data-fancybox","mygallery");
-$(".gallery a").each(function(){
-$(this).attr("data-caption", $(this).find("img").attr("alt"));
-$(this).attr("title", $(this).find("img").attr("alt"));
-});
-
-$(".gallery a").fancybox();
 });
 
 
@@ -97,7 +100,7 @@ sidebar_menu.addEventListener("click", (selectedElement) => {
         let active = sidebar_menu.querySelector(".active");
         active.classList.remove("active");
         selectedElement.target.classList.add("active");
-        if(activeClass)  activeClass.innerHTML = selectedElement.target.innerHTML
+        if (activeClass) activeClass.innerHTML = selectedElement.target.innerHTML
         let filterItems = selectedElement.target.getAttribute("data-name");
         category.forEach((category_item) => {
             let category_filter = category_item.getAttribute("data-name");
